09f58f4e5564b7d3d7e44f80d68f3b0580dbcd35
Talha-Bin-Zubair <talhazubair2125@gmail.com>
2026-02-05 16:48:13 +0500

    Updated

--- backend_project_21/backend/routes/postRoutes.js
+++ backend_project_21/backend/routes/postRoutes.js
@@ -5,13 +5,21 @@ const {
   edit_post,
   delete_post,
   get_posts,
-  edit_draft
+  edit_draft,
+  get_posts_by_id,
+  reject_post,
+  review_post,
+  approve_post,
 } = require("../controllers/postController");
 const { protect } = require("../middlewares/authmiddleware");
 
 router.post("/createpost", protect, create_post);
 router.patch("/editpost/:id", protect, edit_post);
 router.delete("/deletepost/:id", protect, delete_post);
-router.get("/getposts", protect , get_posts);
-router.patch("/editdraft/:id",protect,edit_draft)
+router.get("/getposts", protect, get_posts);
+router.get("/getpost/:id", protect, get_posts_by_id);
+router.patch("/editdraft/:id", protect, edit_draft);
+router.patch("/reject/:id", protect, reject_post);
+router.patch("/reviewdraft/:id", protect, review_post);
+router.patch("/approve/:id", protect, approve_post);
 module.exports = router;
