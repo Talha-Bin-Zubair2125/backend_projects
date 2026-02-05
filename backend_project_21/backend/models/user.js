df97cde3c3f73e382aab0bdaae704927176de3ea
Talha-Bin-Zubair <talhazubair2125@gmail.com>
2026-02-05 17:48:08 +0500

    Updated

--- backend_project_21/backend/models/user.js
+++ backend_project_21/backend/models/user.js
@@ -7,7 +7,7 @@ const user_schema = new mongoose.Schema(
     password: { type: String, required: true },
     role: {
       type: String,
-      enum: ["user","admin", "editor" , "author"],
+      enum: ["admin", "editor" , "author"],
       default: "editor",
     },
   },
